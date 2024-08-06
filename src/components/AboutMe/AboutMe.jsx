ab49aed9cceaf88be21df6b3674eaeeb439688c8
dingras <feracosentino@gmail.com>
2024-08-06 09:03:20 -0300

    Implementacion de Mapa y actualizacion de video

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,34 +5,34 @@ const AboutMe = () => {
         <>
             <h2 className='title-aboutme'>Quienes somos?</h2>
             <div className="card text-bg-dark mx-2 aboutme-container">
-                <img src="/Nosotros.jpeg" className="card-img img-vid-container" alt="..."/>
-                <div className="card-img-overlay d-flex justify-content-center align-items-center">
-                    <h4 className="card-title aboutme-text text-center w-75">
+                <video src="/Soldadura.mp4" className="card-img img-vid-container" autoPlay loop muted></video>
+                <div className="card-img-overlay d-flex justify-content-start align-items-center">
+                    <h4 className="card-title aboutme-text text-start w-50">
                         Soy Manuel Pereyra, me dedico hace mas de 20 años a la herreria de maquinaria pesada, camiones, gruas y mucho mas.
                     </h4>
                 </div>
             </div>
             <div className="card text-bg-dark mx-2 aboutme-container">
                 <img src="/Torneria.png" className="card-img img-vid-container" alt="..."/>
-                <div className="card-img-overlay d-flex justify-content-center align-items-center">
-                    <h4 className="card-title aboutme-text text-center w-75">
-                        En mis años de trabajo no solo me dedique a la herreria, tambien realizo trabajos de torneria desde hace mucho tiempo.
+                <div className="card-img-overlay d-flex justify-content-end align-items-center">
+                    <h4 className="card-title aboutme-text text-end w-50">
+                        En mis años de trabajo no solo me dedique a la herreria, tambien realizo trabajos de torneria e hidraulica desde hace mucho tiempo.
                     </h4>
                 </div>
             </div>
             <div className="card text-bg-dark mx-2 aboutme-container">
                 <video src="/CorteCNC.mp4" className="card-img img-vid-container" autoPlay loop muted></video>
-                <div className="card-img-overlay d-flex justify-content-center align-items-center">
-                    <h4 className="card-title aboutme-text text-center w-75">
-                        Por mi trabajo, siempre tuve que enfrentarme a trabajos en hidraulica, y decidi abrir mi propio local no solo para ofrecer un servicio,
-                        sino tambien para ofrecer productos de calidad y brindar la mejor atencion gracias a todos mis años de experiencia en el rubro.
+                <div className="card-img-overlay d-flex justify-content-start align-items-center">
+                    <h4 className="card-title aboutme-text text-start w-50">
+                        Decidi abrir mi propio local no solo para ofrecer un servicio,
+                        sino tambien para ofrecer productos de calidad y brindar la mejor atencion.
                     </h4>
                 </div>
             </div>
             <div className="card text-bg-dark mx-2 aboutme-container">
                 <img src="/Bienvenida.jpg" className="card-img img-vid-container" alt="..."/>
-                <div className="card-img-overlay d-flex justify-content-center align-items-center">
-                    <h4 className="card-title aboutme-text text-center w-75">
+                <div className="card-img-overlay d-flex justify-content-end align-items-center">
+                    <h4 className="card-title aboutme-text text-end w-50">
                     Esto no es todo, tambien realizamos trabajos de plegado y corte en metales con pantografo CNC.
                     </h4>
                 </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import './Footer.css'
+import dataContact from '../../services/Data/Contacts.json'
 
 const Footer = () => {
     return (
@@ -9,10 +10,20 @@ const Footer = () => {
                     <p>Lunes a Viernes 8 a 18hs.</p>
                     <p>Sabados 8 a 13hs.</p>
                 </div>
-                <div className='col-12 col-md-4 text-center align-self-center'>
-                    <h3>Direccion</h3>
+                <div className="col-12 col-md-4 text-center align-self-center">
+                    <h3>Dirección</h3>
+                    <iframe
+                        className='map-container'
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d563.4689247889858!2d-59.822500802226514!3d-37.67106078264774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1722913347951!5m2!1ses-419!2sar"
+                        width="300"
+                        height="200"
+                        style={{ border: 0 }}
+                        allowFullScreen
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
                     <p>Av. Humaita y Juan Jose Hernandez</p>
-                    <p>Benito Juarez</p>
+                    <p>Benito Juárez</p>
                     <p>Buenos Aires, Argentina</p>
                 </div>
                 <div className='col-12 col-md-4 text-center align-self-center'>
@@ -21,10 +32,16 @@ const Footer = () => {
                     <p>+54 2281 55 3881</p>
                     <p>+54 260 411 0144</p>
                     <div className="btn-group" role="group" aria-label="Basic mixed styles example">
-                        <a href='https://w.app/hidraulicamp' target='_blank' type="button" className="btn btn-lg btn-primary bi bi-whatsapp"></a>
-                        <a href='https://www.instagram.com/hidraulicamanuelpereyra' target='_blank' type="button" className="btn btn-lg btn-primary bi bi-instagram"></a>
-                        <a href='https://www.facebook.com/profile.php?id=100068467522498' target='_blank' type="button" className="btn btn-lg btn-primary bi bi-facebook"></a>
-                        <a href='' type="button" target='_blank' className="btn btn-lg btn-primary bi bi-envelope-at-fill"></a>
+                        {dataContact.map(contact => (
+                            <a
+                                key={contact.id}
+                                href={contact.url}
+                                target='_blank'
+                                rel="noopener noreferrer"
+                                className={`btn btn-lg btn-primary bi ${contact.icon}`}
+                            >
+                            </a>
+                        ))}
                     </div>
                 </div>
             </div>
