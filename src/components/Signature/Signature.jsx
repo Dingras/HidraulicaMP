6201f5497ca73355c3d704235c1b5b19808f3a8a
dingras <feracosentino@gmail.com>
2024-08-11 23:28:12 -0300

    correciones en firma

--- src/components/Signature/Signature.jsx
+++ src/components/Signature/Signature.jsx
@@ -5,7 +5,9 @@ const Signature = () => {
         <div className='signature-container'>
             <p>
                 Esta pagina fue creada por:
-                <a className='signature-link-button' href='https://cosentinodev.vercel.app/'> Cosentino Fernando</a>
+                <a className='signature-link-button' href='https://cosentinodev.vercel.app/' title='Visita el perfil de Cosentino Fernando'>
+                    Cosentino Fernando
+                </a>
             </p>
         </div>
     )
